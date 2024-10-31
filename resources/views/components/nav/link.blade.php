562236c4a13dc39f6564aae63b35f7486fedc8cc
apr0032 <antoniojesusperezruiz1@gmail.com>
2024-10-31 21:25:03 +0100

    menu component

---
+++ resources/views/components/nav/link.blade.php
@@ -0,0 +1,5 @@
+<li>
+    <a href="{{ $href ?? '#' }}" class="text-sm text-gray-800 hover:text-gray-900">
+        {{ $slot }}
+    </a>
+</li>
--- resources/views/examples.blade.php
+++ resources/views/examples.blade.php
@@ -1,6 +1,12 @@
 @extends('template')
 
 @section('content')
+<x-nav class="mb-4">
+    <x-nav.link href="#">Cursos</x-nav.link>
+    <x-nav.link href="#">Planes</x-nav.link>
+    <x-nav.link href="#">Documentos</x-nav.link>
+</x-nav>
+
 <div class="container mx-auto px-4">
     <h1 class="text-3xl">Listado de ejemplos</h1>
     <hr class="my-4">
