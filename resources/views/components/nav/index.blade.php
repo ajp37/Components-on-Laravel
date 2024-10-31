5e5c2c33e8181864ba08484810a4bbab6ac8ca78
apr0032 <antoniojesusperezruiz1@gmail.com>
2024-10-31 21:28:54 +0100

    Document original OpenWebinars code

--- resources/views/components/nav/index.blade.php
+++ resources/views/components/nav/index.blade.php
@@ -1,3 +1,8 @@
+<!-- <nav{{
+    $attributes->merge([
+        'class' => 'py-4 px-6 bg-white flex flex-col sm:flex-row text-center sm:text-left sm:justify-between shadow w-full gap-4 items-center',
+    ])
+}}> -->
 <nav class="py-4 px-6 bg-white flex flex-col sm:flex-row text-center sm:text-left sm:justify-between shadow w-full gap-4 items-center">
     <a href="/" class="text-2xl text-gray-800 hover:text-gray-900">Home</a>
 
--- resources/views/components/nav/link.blade.php
+++ resources/views/components/nav/link.blade.php
@@ -1,4 +1,9 @@
 <li>
+    <!-- <a {{ 
+        $attributes->merge([
+        'class' => 'text-sm text-gray-800 hover:text-gray-900'
+        ]) 
+    }}> -->
     <a href="{{ $href ?? '#' }}" class="text-sm text-gray-800 hover:text-gray-900">
         {{ $slot }}
     </a>
