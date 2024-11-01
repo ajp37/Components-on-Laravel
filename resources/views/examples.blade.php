24ee47b92e6f884abc55b612326e5d9ac76ec65f
apr0032 <antoniojesusperezruiz1@gmail.com>
2024-11-01 17:13:34 +0100

    card component

--- resources/views/examples.blade.php
+++ resources/views/examples.blade.php
@@ -19,5 +19,33 @@
     <x-flash type="error">Mensaje de ERROR</x-flash>
     <x-flash type="info">Mensaje de INFORMACION</x-flash>
     <x-flash type="warning">Mensaje de ADVERTENCIA</x-flash>
+
+    <hr class="my-4">
+    <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
+        <x-card
+            img="https://images.unsplash.com/photo-1726711340790-ccaa3ae7e0c9?"
+            date="Noviembre, 2024"
+            title="Lorem ipsum dolor sit amet"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut libero ultrices ultricies. Nullam nec purus ut libero ultrices ultricies."
+        ></x-card>
+        <x-card
+            img="https://images.unsplash.com/photo-1729974354513-758c8a5d196d?"
+            date="Noviembre, 2024"
+            title="Lorem ipsum dolor sit amet"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut libero ultrices ultricies. Nullam nec purus ut libero ultrices ultricies."
+        ></x-card>
+        <x-card
+            img="https://images.unsplash.com/photo-1728873212984-68b67420bccc?"
+            date="Noviembre, 2024"
+            title="Lorem ipsum dolor sit amet"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut libero ultrices ultricies. Nullam nec purus ut libero ultrices ultricies."
+        ></x-card>
+        <x-card
+            img="https://images.unsplash.com/photo-1730344968874-260056df2de6?"
+            date="Noviembre, 2024"
+            title="Lorem ipsum dolor sit amet"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut libero ultrices ultricies. Nullam nec purus ut libero ultrices ultricies."
+        ></x-card>
+    </div>
 </div>
 @endsection
